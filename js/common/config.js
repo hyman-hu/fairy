69a8d46d771834dbe759ed13800e1474b3cc97ff
hyman.hu-at-752768426100 <hyman.hu@cdskysoft.com>
2018-06-08 17:21:29 +0800

    添加  博客。html

--- js/common/config.js
+++ js/common/config.js
@@ -6,7 +6,10 @@ const FairyBlog = [
     {article:"爱情故事汇总",date:"2018/06/07",content:"想每天给我加仙女讲故事。虽然故事里的男女主角都有属于自己的浪漫故事，但是我也想给我家仙女一辈子的浪漫。记录下来现在自己的想法，这也算提醒以后的自己吧。小伙子好好爱你的仙女吧！"},
     ];
 const LearnBlog = [
-    {article:"Gradle 学习笔记",date:"2018/06/05",content:"Gradle是一个基于Apache Ant和Apache Maven概念的项目自动化建构工具。 它使用一种基于Groovy的特定领域语言来声明项目设置，而不是传统的XML。"},
+    {article:"Gradle 学习笔记",date:"2018/05/28",content:"Gradle是一个基于Apache Ant和Apache Maven概念的项目自动化建构工具。 它使用一种基于Groovy的特定领域语言来声明项目设置，而不是传统的XML。"},
+    {article:"tinkerpop",date:"2018/06/01",content:"tinkerpop gremlin 是图形数据库的一种查询遍历语言。改篇文章是在学习AWS Neptune是整理的图形数据库查询语言Gremlin文档"},
+    {article:"Maven实战学习笔记",date:"2018/06/04",content:"Maven 是一个项目管理工具，主要用于项目构建，依赖管理，项目信息管理。"},
+    {article:"springboot学习笔记",date:"2018/06/05",content:"SpringBoot 学习笔记整理"},
 ];
 const MoodBlog = [
     {article:"Gradle 学习笔记",date:"2018/06/05",content:"Gradle是一个基于Apache Ant和Apache Maven概念的项目自动化建构工具。 它使用一种基于Groovy的特定领域语言来声明项目设置，而不是传统的XML。"},
