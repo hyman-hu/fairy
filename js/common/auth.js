89fe63f96ca325089acb08403869a892d586e40b
hyman.hu-at-752768426100 <hyman.hu@cdskysoft.com>
2018-06-13 19:57:24 +0800

    change cookie

--- js/common/auth.js
+++ js/common/auth.js
@@ -2,23 +2,9 @@
  * 公共部分
  * Created by hyman on 2017/7/5.
  */
-/**
- * 根据name获取cookie的值
- * @param objname
- */
-function getcookie(objname) {//获取指定名称的cookie的值
-    var arrstr = document.cookie.split(";");
-    for (var i = 0; i < arrstr.length; i++) {
-        var temp = arrstr[i].split("=");
-        if (temp[0].trim() == objname) {
-            return unescape(temp[1]);
-        }
-    }
-}
-
-$(function () {
+(function (w, $) {
     //获取cookie中的token  判断权限
-    if (getcookie("userName") !== USERNAME || getcookie("password") !== hex_md5(hex_md5(PASSWORD))) {
+    if ($.cookie("userName") !== USERNAME || $.cookie("password") !== hex_md5(hex_md5(PASSWORD))) {
         alert("您还未登录；请先登录！");
         console.log(window.url);
         let url = window.location.href;
@@ -28,8 +14,10 @@ $(function () {
             window.location.href = "./login.html";
         }
     }
-});
-
+})(window, jQuery);
+/**
+ * 页面跳转
+ */
 function goBack(){
-    window.history.back(-1);
+    window.location.replace(document.referrer);
 }
---
+++ page/hexo搭建静态博客总结笔记.md
@@ -0,0 +1,20 @@
+####什么是Hexo
+- Hexo 是一个快速、简洁且高效的博客框架。Hexo 使用 Markdown（或其他渲染引擎）解析文章，在几秒内，即可利用靓丽的主题生成静态网页。
+####安装
+- 安装前提：
+	- Node.js
+	- Git
+- 安装Hexo：
+	- 全局安装hexo-cli插件：
+		- `npm install -g hexo-cli`
+- 创建Hexo项目初始化：
+> heox init <folder>   //初始化项目
+> 
+> cd <folder>    //进入项目目录
+> 
+> npm install  //初始化npm依赖
+
+- 初始化项目完后的项目结构：
+![](https://i.imgur.com/1xVULY8.gif)
+####_config.yml配置文件
+- 
