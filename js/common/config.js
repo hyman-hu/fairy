d022dcdfa3bfde1aeeba1e44bd9f33632e047070
hyman-hu <18227756560@163.com>
2018-06-26 19:42:57 +0800

    添加博客

--- js/common/config.js
+++ js/common/config.js
@@ -4,13 +4,19 @@ const PASSWORD = "lucky";
 const FairyBlog = [
     {article:"ToFairy",date:"2018/06/05",content:"此篇文章是第一次写给我家小仙女的情书。当时还不知道小仙女的心意，害怕失去她，害怕自己一直只是单相思。所以才写了这篇情书，去表明自己的心意。"},
     {article:"爱情故事汇总",date:"2018/06/07",content:"想每天给我加仙女讲故事。虽然故事里的男女主角都有属于自己的浪漫故事，但是我也想给我家仙女一辈子的浪漫。记录下来现在自己的想法，这也算提醒以后的自己吧。小伙子好好爱你的仙女吧！"},
+    {article:"你淡定的样子真好看",date:"2018/06/15",content:"你淡定的样子真好看,希望我家宝贝一直开心，你淡定的样子真好看"},
+    {article:"当我内心足够强大",date:"2018/06/20",content:"当我内心足够强大  ， 我将一往直前  "},
     ];
 const LearnBlog = [
     {article:"Gradle 学习笔记",date:"2018/05/28",content:"Gradle是一个基于Apache Ant和Apache Maven概念的项目自动化建构工具。 它使用一种基于Groovy的特定领域语言来声明项目设置，而不是传统的XML。"},
     {article:"tinkerpop",date:"2018/06/01",content:"tinkerpop gremlin 是图形数据库的一种查询遍历语言。改篇文章是在学习AWS Neptune是整理的图形数据库查询语言Gremlin文档"},
     {article:"Maven实战学习笔记",date:"2018/06/04",content:"Maven 是一个项目管理工具，主要用于项目构建，依赖管理，项目信息管理。"},
     {article:"springboot学习笔记",date:"2018/06/05",content:"SpringBoot 学习笔记整理"},
+    {article:"H2数据库",date:"2018/06/07",content:"H2 数据库 学习笔记整理"},
+    {article:"hexo搭建静态博客总结笔记",date:"2018/06/12",content:"hexo搭建静态博客总结笔记"},
+    {article:"Java动态代理",date:"2018/06/15",content:"Java动态代理"},
+    {article:"Spring Security4 学习笔记",date:"2018/06/25",content:"Spring Security4 学习笔记"},
 ];
 const MoodBlog = [
-    {article:"Gradle 学习笔记",date:"2018/06/05",content:"Gradle是一个基于Apache Ant和Apache Maven概念的项目自动化建构工具。 它使用一种基于Groovy的特定领域语言来声明项目设置，而不是传统的XML。"},
+    {article:"周末菜肴",date:"2018/06/05",content:"周末菜肴 学习使人进步"},
 ];
